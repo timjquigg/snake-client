4959d4ab03c8220498f5a5bc22ad3f9accc39e03
Tim Quigg <timjquigg@gmail.com>
2022-10-26 13:57:35 -0600

    refactored by moving the setupInput function to a separate module

---
+++ input.js
@@ -0,0 +1,20 @@
+const handleUserInput = (data) => {
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+module.exports = {
+  setupInput
+};
--- play.js
+++ play.js
@@ -1,23 +1,9 @@
 //const net = require("net");
 const {connect} = require('./client');
+const {setupInput} = require('./input');
 
-const handleUserInput = (data) => {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-setupInput();
 console.log("Connecting ...");
 connect();
+
+setupInput();
