372c3f064a47a6b1b6e85d1db7ded89969646940
Tim Quigg <timjquigg@gmail.com>
2022-10-26 13:54:09 -0600

    created function to handle user input

--- play.js
+++ play.js
@@ -1,5 +1,23 @@
 //const net = require("net");
 const {connect} = require('./client');
 
+const handleUserInput = (data) => {
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+setupInput();
 console.log("Connecting ...");
 connect();
