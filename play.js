eea9b5183e0426b6f6c9800503856424c17fc158
Tim Quigg <timjquigg@gmail.com>
2022-10-26 13:33:14 -0600

    Refactored by moving connect function to separate connect.js

--- play.js
+++ play.js
@@ -1,21 +1,5 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',   // IP address here,
-    port: '50541'             // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
+//const net = require("net");
+const {connect} = require('./client');
 
 console.log("Connecting ...");
 connect();
