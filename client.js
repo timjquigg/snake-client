70116a04552b9d8dbe99bb8cb70d66d550e4856c
Tim Quigg <timjquigg@gmail.com>
2022-10-26 13:40:19 -0600

    Now sends name to server upon connection

--- client.js
+++ client.js
@@ -9,6 +9,11 @@ const connect = function() {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  
+  conn.on("connect", () => {
+    console.log('successfully coonnected to server!');
+    conn.write('Name: Tim');
+  });
 
   conn.on('data', (data) => {
     console.log(data);
